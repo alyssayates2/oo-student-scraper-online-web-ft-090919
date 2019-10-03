c952e56e2d339be7379b2e0df31316e34eb83884
Alyssa Yates <bosslys@icloud.com>
2019-10-03 14:32:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,22 @@
 require 'open-uri'
 require 'pry'
 
+require_relative './student.rb'
+
 class Scraper
 
+
   def self.scrape_index_page(index_url)
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
 
+
+#name //// doc.css(".roster").first.css("h4").text
+# location //// doc.css(".roster").first.css(".student-location").text
+# profile /// doc.css(".roster").first.css(".view-profile-div").text
