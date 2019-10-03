6d20f8669bcaed9234eb6dffd10b3902437f6683
Alyssa Yates <bosslys@icloud.com>
2019-10-03 14:35:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,27 +1,22 @@
-@@all = []
+class Student
 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+
+  @@all = []
 
   def initialize(student_hash)
 
-    student_hash.each {|k,v| self.send(("#{k}="),v)}
-    @@all << self
- end
+  end
 
+  def self.create_from_collection(students_array)
 
- def self.create_from_collection(students_array)
+  end
 
-    students_array.each{|student| Student.new(student)}
+  def add_student_attributes(attributes_hash)
+
+  end
+
+  def self.all
+
+  end
 end
-
-
-def add_student_attributes(attributes_hash)
-
-    attributes_hash.each{|k,v| self.send(("#{k}="),v)}
-  end
-
-
-def self.all
-
-    @@all
-  end
-	end
